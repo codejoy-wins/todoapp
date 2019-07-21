a86855abb253ac7d2a9ad3200f26c0bf8d5e6c9f
Max <mjann@ucsc.edu>
2019-07-21 10:19:04 -0700

    labels and padding

--- src/Todo.js
+++ src/Todo.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Todo(props){
     return (
-        <div>
-            <input type="checkbox" checked = {props.completed} onChange = {()=>props.handleChange(props.id)}></input>
-            {props.desc}
+        <div id="todo">
+            <input id={props.id} type="checkbox" checked = {props.completed} onChange = {()=>props.handleChange(props.id)}></input>
+            <label for={props.id}>{props.desc}</label>
         </div>
     )
 }
