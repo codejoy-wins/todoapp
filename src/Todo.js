6f84cc365adaf5f4c84adb2d2f89d38ad0f36e4d
Max <mjann@ucsc.edu>
2019-07-24 16:49:05 -0700

    golden accomplishments

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 function Todo(props){
+    const MySty = {
+        color: "gold",
+    }
     return (
         <div id="todo">
             <input id={props.id} type="checkbox" checked = {props.completed} onChange = {()=>props.handleChange(props.id)}></input>
-            <label for={props.id}>{props.desc}</label>
+            <label style={props.completed ? MySty : null} for={props.id}>{props.desc}</label>
         </div>
     )
 }
